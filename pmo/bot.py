a339797e11bcf97bc1e090cbc86a65c41200a933
lumix103 <fabiant941@gmail.com>
2022-08-24 21:01:15 -0700

    made changes and added a profanity filter

--- pmo/bot.py
+++ pmo/bot.py
@@ -2,6 +2,11 @@ import discord
 import os
 from dotenv import load_dotenv
 
+bad_words = []
+
+with open('pmo\\bad_words.txt') as file:
+    bad_words = file.readlines()
+    bad_words = [word.strip() for word in bad_words]
 
 class PMO(discord.Client):
     async def on_ready(self):
@@ -9,10 +14,18 @@ class PMO(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        msg_ctx = message.content.strip().lower()
+        if [i for i in bad_words if i in msg_ctx]:
+            await message.delete()
+            await message.channel.send(
+                    "That's a bad word. Please dont say more bad words"
+                    )
 
 
 def main():
     load_dotenv()
+    
+    print(bad_words)
 
     intents = discord.Intents.default()
 
